476ac5e1c3af878e44b94a37221e79012be8d9a3
Haobijam Chetan Singh <chetanhaobijam@gmail.com>
2021-10-14 16:12:49 +0530

    Finished Styling Navigation Bar

--- main.js
+++ main.js
@@ -1,13 +1,26 @@
 // Variable Declarations
 const navGallery = document.querySelector("#nav-gallery");
 const navDropDown = document.querySelector(".nav-dropdown");
+const navUl = document.querySelector(".nav-ul");
+const collapseIcon = document.querySelector(".collapse-icon");
+const closeNav = document.querySelector(".close-nav");
 
 // Toggle display Nav Gallery Dropdown
-navGallery.addEventListener("mouseenter", () => {
-  navDropDown.classList.add("show");
+navGallery.addEventListener("click", () => {
+  navDropDown.classList.toggle("show");
 })
 
-navDropDown.addEventListener("mouseleave", () => {
-  navDropDown.classList.remove("show");
+// Toggle Navbar
+collapseIcon.addEventListener("click", () => {
+  navUl.classList.add("flex-show");
+  closeNav.classList.add("show");
 })
+
+// Close Navbar
+closeNav.addEventListener("click", () => {
+  navUl.classList.remove("flex-show");
+  closeNav.classList.remove("show");
+})
+
+
 
