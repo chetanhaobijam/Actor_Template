ffb0f4b477866270a075c43d93f2bfcd7966444f
Haobijam Chetan Singh <chetanhaobijam@gmail.com>
2021-10-23 15:42:15 +0530

    Added  Styles to Gallery Section

--- main.js
+++ main.js
@@ -4,6 +4,9 @@ const navDropDown = document.querySelector(".nav-dropdown");
 const navUl = document.querySelector(".nav-ul");
 const collapseIcon = document.querySelector(".collapse-icon");
 const closeNav = document.querySelector(".close-nav");
+const galleryItem = document.querySelectorAll(".gallery-item");
+const galleryImage = document.querySelectorAll(".gallery-item img");
+const galleryMenus = document.querySelectorAll(".gallery-menus");
 
 // Toggle display Nav Gallery Dropdown
 navGallery.addEventListener("click", () => {
@@ -36,4 +39,15 @@ $(document).ready(function(){
   });
 });
 
+// Toggle Display of Gallery Menus when hover on Gallery Item
+for(let i = 0; i < galleryImage.length; i++) {
+  galleryItem[i].addEventListener("mouseenter", () => {
+    galleryImage[i].classList.add("image-hover");
+    galleryMenus[i].classList.add("show");
+  })
 
+  galleryItem[i].addEventListener("mouseleave", () => {
+    galleryImage[i].classList.remove("image-hover");
+    galleryMenus[i].classList.remove("show");
+  })
+}
