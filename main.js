657de55f59d5fa75e4f7ad68292246eb6dbbc073
Haobijam Chetan Singh <chetanhaobijam@gmail.com>
2021-10-26 14:25:47 +0530

    Finished Home Page

--- main.js
+++ main.js
@@ -51,7 +51,7 @@ $(document).ready(function(){
     loop: true,
     nav: true,
     dots: true,
-    autoplay: true,
+    autoplay: false,
     autoplaySpeed: 1000,
     smartSpeed: 1500,
     autoplayHoverPause: true
